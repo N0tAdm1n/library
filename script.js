ca464c0ebdd84eb29e881b017e4a08ee36781981
N0tAdm1n <n0tadm1n29@gmail.com>
2022-03-13 13:38:53 +0530

    Format and Comment script.js

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
 let myLibrary = [];
 
 function Book(title, author, pageNo, readStatus) {
-    this.title = title;
-    this.author = author;
-    this.pageNo = pageNo;
-    this.readStatus = readStatus;
-    this.info = function() {
-        return `${this.title} by ${this.author}, ${this.pageNo}, ${readStatus}`;
-    }
+  this.title = title;
+  this.author = author;
+  this.pageNo = pageNo;
+  this.readStatus = readStatus;
+  this.info = function () {
+    return `${this.title} by ${this.author}, ${this.pageNo}, ${readStatus}`;
+  };
 }
-
+//addBook will add a book at the end of the myLibrary[]
 function addBook(title, author, pageNo, readStatus) {
-    myLibrary.push(Book(title,author,pageNo,readStatus));
+  myLibrary.push(new Book(title, author, pageNo, readStatus));
 }
