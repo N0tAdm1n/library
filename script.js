418b568b851262677cc78af2a2745894580cf4f8
N0tAdm1n <n0tadm1n29@gmail.com>
2022-03-13 14:18:53 +0530

    Add checkLibrary() and Fix Book.prototype

--- script.js
+++ script.js
@@ -5,11 +5,17 @@ function Book(title, author, pageNo, readStatus) {
   this.author = author;
   this.pageNo = pageNo;
   this.readStatus = readStatus;
-  this.info = function () {
-    return `${this.title} by ${this.author}, ${this.pageNo}, ${readStatus}`;
-  };
 }
+Book.prototype.info = function () {
+  return `${this.title} by ${this.author}, ${this.pageNo}, ${this.readStatus}`;
+};
 //addBook will add a book at the end of the myLibrary[]
 function addBook(title, author, pageNo, readStatus) {
   myLibrary.push(new Book(title, author, pageNo, readStatus));
+}
+//to loop every book in myLibrary
+function checkLibrary() {
+  for (const book of myLibrary) {
+    console.log(book.info());
+  }
 }
