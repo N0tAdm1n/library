6b8e6837bc8a1226abe0c9028d64e464323b8398
N0tAdm1n <n0tadm1n29@gmail.com>
2022-03-19 14:56:00 +0530

    Fix elements name

--- script.js
+++ script.js
@@ -47,19 +47,19 @@ function updateLibrary() {
 
   const author = document.createElement("div");
   author.textContent = `${myLibrary[myLibrary.length - 1].author}`;
-  title.classList.add("author");
+  author.classList.add("author");
   bookTile.appendChild(author);
 
   const pageNo = document.createElement("div");
   pageNo.textContent = `${myLibrary[myLibrary.length - 1].pageNo} pages`;
-  title.classList.add("pageNo");
+  pageNo.classList.add("pageNo");
   bookTile.appendChild(pageNo);
 
   const readStatus = document.createElement("div");
   readStatus.textContent = myLibrary[myLibrary.length - 1].readStatus
     ? `Completed`
     : `Not Started`;
-  title.classList.add("readStatus");
+  readStatus.classList.add("readStatus");
   bookTile.appendChild(readStatus);
 
   bookTile.classList.add("tile");
