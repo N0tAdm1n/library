9ec5044fd6eb737421ed94ca1e0fdbd501827b94
N0tAdm1n <n0tadm1n29@gmail.com>
2022-04-09 15:25:56 +0530

    Refactor to ES6 class

--- script.js
+++ script.js
@@ -1,14 +1,16 @@
 let myLibrary = [];
 
-function Book(title, author, pageNo, readStatus) {
-  this.title = title;
-  this.author = author;
-  this.pageNo = pageNo;
-  this.readStatus = readStatus;
+class Book {
+  constructor(title, author, pageNo, readStatus) {
+    this.title = title;
+    this.author = author;
+    this.pageNo = pageNo;
+    this.readStatus = readStatus;
+  }
+  info() {
+    return `${this.title} by ${this.author}, ${this.pageNo}, ${this.readStatus}`;
+  }
 }
-Book.prototype.info = function () {
-  return `${this.title} by ${this.author}, ${this.pageNo}, ${this.readStatus}`;
-};
 //addBook will add a book at the end of the myLibrary[]
 function addBook(title, author, pageNo, readStatus) {
   myLibrary.push(new Book(title, author, pageNo, readStatus));
